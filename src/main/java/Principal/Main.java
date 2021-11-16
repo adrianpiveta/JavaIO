f61eb3fb429969ce66a485b76eb5c9eaaf4c6d9b
adrianpiveta <danielpiveta257@gmail.com>
2021-11-16 17:52:29 -0300

    Fazendo leitura com InputStream

--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -4,10 +4,40 @@
  */
 package Principal;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author danie
  */
 public class Main {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        InputStream	is	=	new	FileInputStream("D:/NetBeansProjects/JavaIO/src/main/java/Principal/teste.txt");
+        InputStreamReader	isr	=	new	InputStreamReader(is);
+	BufferedReader	br	=	new	BufferedReader(isr);
+	String	s	=	br.readLine();	//	primeira	linha
+	while	(s	!=	null)	{
+            System.out.println(s);
+            s=br.readLine();
+        }
+        br.close();
+        
+        //teste com entrada do teclado
+        is	=	System.in;
+        isr	=	new	InputStreamReader(is);
+        br	=	new	BufferedReader(isr);
+        s	=	br.readLine();
+        while	(s	!=	null)	{
+            System.out.println(s);
+            s	=	br.readLine();
+        }
+        br.close();
+
+    }
     
 }
